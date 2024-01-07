9d4ceb2e36b6b8b714fc0d60cd703b1d443f19d3
Hugo_Oliviria <147427922+Alla21-dev@users.noreply.github.com>
2024-01-07 16:16:40 +0100

    Add files via upload

---
+++ partials-front/footer.php
@@ -0,0 +1,12 @@
+
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">Guerriche alla abd elmoumene</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
